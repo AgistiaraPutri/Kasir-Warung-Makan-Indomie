a5fa05a753c51630434c1049cc2f3810a9743380
Agistiara Putri <101690147+AgistiaraPutri@users.noreply.github.com>
2022-12-27 19:53:16 +0700

    Add files via upload

---
+++ pbo-tugasakhir/src/Kasir.java
@@ -0,0 +1,13 @@
+public class Kasir {
+    public static void main(String[] args) {
+
+        DataMenu mn  = new DataMenu();
+        System.out.println("\t\t==========================");
+        System.out.println("\t\t     WARMINDO AGIS'S      ");
+        System.out.println("\t\t==========================");
+
+        mn.menu();
+        mn.proses();
+        mn.run();
+    }
+}
---
+++ pbo-tugasakhir/src/Transaksi.java
@@ -0,0 +1,15 @@
+public abstract class Transaksi {
+
+    protected int jumlah, bayar, tunai, kembalian, tqty;
+
+    int no[] = new int[50];
+    int qty[] = new int[50];
+    int harga[] = new int[50];
+    int total[] = new int[50];
+    String namamenu[] = new String[50];
+
+    abstract void menu();
+    abstract void proses();
+    abstract void run();
+
+}
